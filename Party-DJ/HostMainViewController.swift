b7feb866a0d5a68ad145a589b790fe3b2bd3b191
Shana <sjoseph0914@gmail.com>
2017-12-08 10:59:01 -0500

    l

--- Party-DJ/HostMainViewController.swift
+++ Party-DJ/HostMainViewController.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+import Alamofire
+import AlamofireImage
+
+class HostMainViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var defaultProfileView: UIView!
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    
+    @IBOutlet weak var joinPlaylistButton: UIButton!
+    @IBOutlet weak var createPlaylistButton: UIButton!
+    
+ 
+  
+    
+   
+    
+    
+
+    
+}
+
